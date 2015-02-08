a7bbe7738b5f5eb6e75c149d67ae0208e00217c0
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-02-08 15:24:44 -0800

    readability

--- bin/verification.sh
+++ bin/verification.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-echo "\nPHP-FPM running:\n"
+echo "PHP-FPM running:"
 ps aux | grep php-fpm | sed 's/^/    /'
 
-echo "\nNGINX running:\n"
+echo "NGINX running:"
 /etc/init.d/nginx status | sed 's/^/    /'
 
-echo "\nGIT version:\n"
+echo "GIT version:"
 git --version | sed 's/^/    /'
 
-echo "\nMYSQL running:\n"
+echo "MYSQL running:"
 mysqladmin -uroot status  | sed 's/^/    /'
 
-echo "\nMONGO running:\n"
+echo "MONGO running:"
 status mongodb | sed 's/^/    /'
 
-echo "\nSHELL type:\n"
+echo "SHELL type:"
 echo $SHELL | sed 's/^/    /'
 
-echo "\nNODE version:\n"
+echo "NODE version:"
 node --version | sed 's/^/    /'
 
-echo "\nNPM version:\n"
+echo "NPM version:"
 npm --version | sed 's/^/    /'
